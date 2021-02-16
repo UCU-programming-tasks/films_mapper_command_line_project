679ad4c657c219fdc35d1c9e7c9d480881ed62f5
dyaroshevych <dyaroshevych@gmail.com>
2021-02-16 22:15:00 +0200

    Put the code from the main module into a function

--- main.py
+++ main.py
@@ -4,23 +4,32 @@ from geopy.extra.rate_limiter import RateLimiter
 from geopy.geocoders import Nominatim
 from map_creation import main as create_map
 
-year = int(input('Please enter a year you would like to have a map for: '))
-coordinates = tuple(
-    map(float, input('Please enter your location (format: lat long): ').split(' ')))
-print('Locations are getting collected...')
 
-locator = Nominatim(user_agent='dyaroshevych111')
-geocode = RateLimiter(locator.geocode, min_delay_seconds=1)
+def main():
+    """
+    Main function for collecting user data and generating the map.
+    """
+    year = int(input('Please enter a year you would like to have a map for: '))
+    coordinates = tuple(
+        map(float, input('Please enter your location (format: lat long): ').split(' ')))
+    print('Locations are getting collected...')
 
-USER_COUNTRY = locator.reverse(coordinates, language='en')[-2].split(", ")[-1]
+    locator = Nominatim(user_agent='dyaroshevych111')
+    geocode = RateLimiter(locator.geocode, min_delay_seconds=1)
 
-if USER_COUNTRY == 'United States':
-    USER_COUNTRY = 'USA'
+    USER_COUNTRY = locator.reverse(
+        coordinates, language='en')[-2].split(", ")[-1]
 
-df = pd.read_csv('locations.csv')
-df = df[df['Country'] == USER_COUNTRY]
+    if USER_COUNTRY == 'United States':
+        USER_COUNTRY = 'USA'
+
+    df = pd.read_csv('locations.csv')
+    df = df[(df['Country'] == USER_COUNTRY) & (df['Year'] == year)]
+
+    print('Map is generating...')
+    create_map(df, coordinates, geocode)
+    print('Finished. Please have look at the map films_map.html')
 
 
-print('Map is generating...')
-create_map(df, coordinates, geocode)
-print('Finished. Please have look at the map films_map.html')
+if __name__ == '__main__':
+    main()
